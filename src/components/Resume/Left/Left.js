aa9100dcf66fd6e3a2673dec0a4dc7f9f73494f3
gabemanfroi <gabsmanfroi@gmail.com>
2021-08-01 22:34:52 -0300

    💄 Added Portfolio Section

--- src/components/Resume/Left/Left.js
+++ src/components/Resume/Left/Left.js
@@ -1,8 +1,6 @@
 import { Link } from '@material-ui/core';
 import { GetApp, TurnedIn } from '@material-ui/icons';
 import Person from '@material-ui/icons/Person';
-import PhoneIcon from '@material-ui/icons/Phone';
-import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import { useContext } from 'react';
 import { CurrentSectionContext } from '../../../contexts/CurrentSectionContext';
 import classes from './Left.module.scss';
@@ -18,12 +16,6 @@ export default function Left() {
                     </li>
                     <li>
                         <TurnedIn fontSize='large' onClick={() => setCurrentSection(1)} />
-                    </li>
-                    <li>
-                        <TrendingUpIcon fontSize='large' onClick={() => setCurrentSection(2)} />
-                    </li>
-                    <li>
-                        <PhoneIcon fontSize='large' onClick={() => setCurrentSection(3)} />
                     </li>
                 </ul>
                 <Link href='/assets/docs/Gabriel_Manfroi_Resume_4.pdf' download>
--- src/components/Resume/Right/Portfolio/Portfolio.js
+++ src/components/Resume/Right/Portfolio/Portfolio.js
@@ -1,3 +1,58 @@
+import { Link } from '@material-ui/core';
+import classes from './Portfolio.module.scss';
 export default function Portfolio() {
-    return <div>Portfolio</div>;
+    return (
+        <section className={classes.PortfolioSection}>
+            <div className={classes.PortfolioContainer}>
+                <div className={classes.PotfolioItem}>
+                    <Link href='https://seclab-website.vercel.app/' target='_blank'>
+                        <img
+                            src='https://seclab-website.vercel.app/static/media/Camada.2f0542e9.png'
+                            alt='Projeto Seclab'
+                        />
+                    </Link>
+                </div>
+                <div className={classes.PotfolioItem}>
+                    <Link href='https://github.com/gabemanfroi/star-wars-crew' target='_blank'>
+                        <img
+                            src='https://github.githubassets.com/images/modules/logos_page/Octocat.png'
+                            alt='Projeto Seclab'
+                        />
+                    </Link>
+                </div>
+                <div className={classes.PotfolioItem}>
+                    <Link href='https://github.com/gabemanfroi/PythonSerialReader' target='_blank'>
+                        <img
+                            src='https://github.githubassets.com/images/modules/logos_page/Octocat.png'
+                            alt='Projeto Seclab'
+                        />
+                    </Link>
+                </div>
+                <div className={classes.PotfolioItem}>
+                    <Link href='https://campfire-landscape.vercel.app/' target='_blank'>
+                        <img
+                            src='https://static.vecteezy.com/system/resources/previews/000/208/942/non_2x/vector-beautiful-camping-illustration.jpg'
+                            alt='Projeto Seclab'
+                        />
+                    </Link>
+                </div>
+                <div className={classes.PotfolioItem}>
+                    <Link href='https://bmengenharia.eng.br' target='_blank'>
+                        <img
+                            src='https://bmengenharia.eng.br/media/architect-3979490_FRJGOhS.jpg'
+                            alt='Projeto BM Engenharia'
+                        />
+                    </Link>
+                </div>
+                <div className={classes.PotfolioItem}>
+                    <Link href='https://proservpb.com.br' target='_blank'>
+                        <img
+                            src='https://proservpb.com.br/static/website/img/hero-bg.jpg'
+                            alt='Projeto Proserv & Solution'
+                        />
+                    </Link>
+                </div>
+            </div>
+        </section>
+    );
 }
