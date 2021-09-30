c112f8879af4a07d055f1d8b18674a0eabf3c3b1
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-09-29 23:35:16 -0300

    💄 Added Personal Photo and some other skills

--- src/components/Resume/Right/About/ProgrammingLanguagesSection/ProgrammingLanguagesSection.js
+++ src/components/Resume/Right/About/ProgrammingLanguagesSection/ProgrammingLanguagesSection.js
@@ -2,20 +2,22 @@ import ProgrammingLanguageItem from './ProgrammingLanguageItem/ProgrammingLangua
 import classes from './ProgrammingLanguagesSection.module.scss';
 
 export default function ProgrammingLanguagesSection() {
-    return (
-        <section className={classes.ProgrammingLanguagesSection}>
-            <h2>Software</h2>
-            <div className={classes.ProgrammingLanguagesContainer}>
-                <ProgrammingLanguageItem programmingLanguageTitle={'JavaScript'} proficiency={5} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'React'} proficiency={5} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'Vue'} proficiency={5} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'Python'} proficiency={5} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'Flutter'} proficiency={4} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'Node'} proficiency={4} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'MySQL'} proficiency={5} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'MongoDB'} proficiency={3} />
-                <ProgrammingLanguageItem programmingLanguageTitle={'REST API'} proficiency={5} />
-            </div>
-        </section>
-    );
+  return (
+    <section className={classes.ProgrammingLanguagesSection}>
+      <h2>Software</h2>
+      <div className={classes.ProgrammingLanguagesContainer}>
+        <ProgrammingLanguageItem programmingLanguageTitle={'JavaScript'} proficiency={5} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'TypesScript'} proficiency={4} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'React'} proficiency={5} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'Vue'} proficiency={5} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'Python'} proficiency={5} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'Flutter'} proficiency={4} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'Node'} proficiency={4} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'MySQL'} proficiency={5} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'MongoDB'} proficiency={4} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'GIT'} proficiency={5} />
+        <ProgrammingLanguageItem programmingLanguageTitle={'REST API'} proficiency={5} />
+      </div>
+    </section>
+  );
 }
