68d5a24bcc500e268af7399fdd97687d7af86992
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-09-29 23:46:32 -0300

    💄 Now Nav Icons colors responds to page changing

--- src/components/Resume/Left/Left.js
+++ src/components/Resume/Left/Left.js
@@ -5,23 +5,35 @@ import { useContext } from 'react';
 import { CurrentSectionContext } from '../../../contexts/CurrentSectionContext';
 import classes from './Left.module.scss';
 export default function Left() {
-    const { setCurrentSection } = useContext(CurrentSectionContext);
+  const { currentSection, setCurrentSection } = useContext(CurrentSectionContext);
 
-    return (
-        <div className={classes.LeftContainer}>
-            <nav className={classes.Navbar}>
-                <ul>
-                    <li>
-                        <Person className={classes.active} fontSize='large' onClick={() => setCurrentSection(0)} />
-                    </li>
-                    <li>
-                        <TurnedIn fontSize='large' onClick={() => setCurrentSection(1)} />
-                    </li>
-                </ul>
-                <Link href='/assets/docs/Gabriel_Manfroi_Resume_4.pdf' download>
-                    <GetApp fontSize='large' />
-                </Link>
-            </nav>
-        </div>
-    );
+  return (
+    <div className={classes.LeftContainer}>
+      <nav className={classes.Navbar}>
+        <ul>
+          <li>
+            <Person
+              className={currentSection === 0 && classes.active}
+              fontSize='large'
+              onClick={() => {
+                setCurrentSection(0);
+              }}
+            />
+          </li>
+          <li>
+            <TurnedIn
+              className={currentSection === 1 && classes.active}
+              fontSize='large'
+              onClick={() => {
+                setCurrentSection(1);
+              }}
+            />
+          </li>
+        </ul>
+        <Link href='/assets/docs/Gabriel_Manfroi_Resume_4.pdf' download>
+          <GetApp fontSize='large' />
+        </Link>
+      </nav>
+    </div>
+  );
 }
